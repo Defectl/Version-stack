8b535cd229d459f242dc00d07ef6300a032640d7
Defectl <101496751+Defectl@users.noreply.github.com>
2022-03-13 12:50:35 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SQ7
+{
+    public class ActionHistory<T>
+    {
+        private readonly LinkedList<T> actionHistory = new LinkedList<T>();
+
+        public void Push(T item) => actionHistory.AddLast(item);
+
+        public T Pop()
+        {
+            var currentItem = actionHistory.Last.Value;
+            actionHistory.RemoveLast();
+            return currentItem;
+        }
+
+        public int Count => actionHistory.Count;
+
+        public void Clear() => actionHistory.Clear();
+    }
+
+    public class Stack<T>
+    {
+        private readonly ActionHistory<IAction<T>> actionHistory =
+            new ActionHistory<IAction<T>>();
+
+        public List<T> ListItem = new List<T>();
+
+        public void Push(T item)
+        {
+            var command = new Adding<T>(item);
+            actionHistory.Push(command);
+            command.Execute(ListItem);
+        }
+
+        public void Pop()
+        {
+            var command = new Removing<T> { Index = actionHistory.Count };
+            actionHistory.Push(command);
+            command.Execute(ListItem);
+        }
+
+        public void Forget() => actionHistory.Clear();
+
+        /*public void Rollback(int statusIndex)
+        {
+            var rollbackHictory = new ActionHistory<IAction<T>>();
+            for (int i = actionHistory.Count - 1; i > statusIndex; i--)
+            {
+                var command = actionHistory.Pop();
+                command.Undo(ListItem);
+                rollbackHictory.Push(command);
+            }
+
+        }*/
+    }
+
+    interface IAction<T>
+    {
+        void Execute(List<T> items);
+        void Undo(List<T> items);
+    }
+
+    /*public class Rollbacking<T> : IAction<T>
+    {
+        public readonly int StatusIndex;
+        public readonly LinkedList<T> ActionHistory;
+        private readonly ActionHistory<IAction<T>> historyRollback;
+
+        public Rollbacking(int statusIndex, LinkedList<T> actionHistory, List<T> listItem)
+        {
+            this.StatusIndex = statusIndex;
+            this.ActionHistory = actionHistory;
+            historyRollback = new ActionHistory<IAction<T>>();
+        }
+
+        public void Execute(List<T> items)
+        {
+
+        }
+
+        public void Undo(List<T> items)
+        {
+            throw new NotImplementedException();
+        }
+    }*/
+
+    public class Adding<T> : IAction<T>
+    {
+        public readonly T Item;
+
+        public Adding(T item)
+        {
+            this.Item = item;
+        }
+
+        public void Undo(List<T> items)
+        {
+            items.Remove(Item);
+        }
+
+        public void Execute(List<T> items)
+        {
+            items.Add(Item);
+        }
+    }
+
+    public class Removing<T> : IAction<T>
+    {
+        public T Item;
+        public int Index;
+
+        public void Undo(List<T> items)
+        {
+            items.Add(Item);
+        }
+
+        public void Execute(List<T> items)
+        {
+            Item = items[Index];
+            items.RemoveAt(Index);
+        }
+    }
+}
