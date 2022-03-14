9db855e77e8cf06d17117a8e9920eb5c31e0bc69
Defectl <101496751+Defectl@users.noreply.github.com>
2022-03-14 18:28:01 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,45 +11,20 @@ namespace SQ7
 {
     public class ActionHistory<T>
     {
-        private Example<T> last;
-        private readonly List<Example<T>> actionHistory = new List<Example<T>>();
+        private readonly LinkedList<T> actionHistory = new LinkedList<T>();
 
-        public void Push(T value)
-        {
-            last = new Example<T>(value, last);
-            actionHistory.Add(last);
-        }
+        public void Push(T item) => actionHistory.AddLast(item);
 
         public T Pop()
         {
-            var value = last.Value;
-            last = last.Previous;
-            actionHistory.RemoveAt(Count);
-            return value;
+            var currentItem = actionHistory.Last.Value;
+            actionHistory.RemoveLast();
+            return currentItem;
         }
-
-        /*public void Rollback(int statusIndex)
-        {
-            last = actionHistory[statusIndex];
-            actionHistory.Clear();
-            actionHistory.Add(last);
-        }*/
-
-        public void Clear() => last = null;
 
         public int Count => actionHistory.Count;
-    }
 
-    public class Example<T>
-    {
-        public T Value;
-        public Example<T> Previous;
-
-        public Example(T value, Example<T> previous)
-        {
-            Value = value;
-            Previous = previous;
-        }
+        public void Clear() => actionHistory.Clear();
     }
 
     public class Stack<T>
@@ -68,17 +43,20 @@ namespace SQ7
 
         public void Pop()
         {
-            var command = new Removing<T> { Index = actionHistory.Count };
-            actionHistory.Push(command);
-            command.Execute(ListItem);
+                var command = new Removing<T>();
+                actionHistory.Push(command);
+                command.Execute(ListItem);
         }
 
         public void Forget() => actionHistory.Clear();
 
         public void Rollback(int statusIndex)
         {
-      
-
+            for (int i = actionHistory.Count; i > statusIndex; i--)
+            {
+                var command = actionHistory.Pop();
+                command.Undo(ListItem);
+            }
         }
     }
 
@@ -111,7 +89,6 @@ namespace SQ7
     public class Removing<T> : IAction<T>
     {
         public T Item;
-        public int Index;
 
         public void Undo(List<T> items)
         {
@@ -120,8 +97,16 @@ namespace SQ7
 
         public void Execute(List<T> items)
         {
-            Item = items[Index];
-            items.RemoveAt(Index);
+            Item = items[items.Count - 1];
+            items.RemoveAt(items.Count - 1);
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+
         }
     }
 }
